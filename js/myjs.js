f0aac7bfe3dcba6700770b4cbdad598f9a0dfc04
Oksana <ksenix_p@mail.ru>
2017-12-27 22:37:45 +0300

    SecondCommit
    Add Menu Functionality

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,32 @@
+/*----------------Кнопка В Каталог--------------------*/
+
+$(function() {
+
+$(window).scroll(function() {
+
+if($(this).scrollTop() != 0) {
+
+$('#toCatalog').fadeIn();
+
+} else {
+
+$('#toCatalog').fadeOut();
+
+}
+
+});
+
+$('#toCatalog').click(function() {
+
+$('body,html').animate({scrollTop:0},800);
+$('body,html').find('#collapseOne').addClass('show');
+
+});
+
+});
+
+/*----------------Кнопка Наверх--------------------*/
+
 $(function() {
 
 $(window).scroll(function() {
@@ -18,6 +47,19 @@ $('#toTop').click(function() {
 
 $('body,html').animate({scrollTop:0},800);
 
+
 });
 
+});
+
+/*------------------Раскрытие меню на главной странице----------------------*/
+$(document).ready(function() {
+  var location = window.location.href;
+  var cur_url = location.split('/').pop();
+  var cur_link = "index.html";
+
+  if (cur_url == cur_link) {
+
+    $('#collapseOne').addClass('show');
+  }
 });
